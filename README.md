2e1fbb501089a635c9e8ee426f95b0c87998b798
Michal Repec <mikerepec@gmail.com>
2019-11-06 22:29:26 +0100

    Countdown setup

--- README.md
+++ README.md
@@ -1,29 +1,5 @@
-# dt
+# Death Stranding release countdown
 
-## Project setup
-```
-yarn install
-```
+Check it out [here](https://en.wikipedia.org/wiki/Death_Stranding)
 
-### Compiles and hot-reloads for development
-```
-yarn run serve
-```
-
-### Compiles and minifies for production
-```
-yarn run build
-```
-
-### Run your tests
-```
-yarn run test
-```
-
-### Lints and fixes files
-```
-yarn run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+About [Death Stranding](https://en.wikipedia.org/wiki/Death_Stranding)
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" :class="{ in: fadeInApp }">
+    <div class="countdown-wrap" :class="{ in: fadeInCount }">
+      <span class="time">{{ timer }}</span>
+    </div>
+    <div class="copy">
+      This is a fan site, all rights to Death Stranding belong fully to &copy;
+      <a href="http://kojimaproductions.jp">Kojima Productions</a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { DateTime } from "luxon"
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
-};
+  data() {
+    return {
+      fadeInApp: false,
+      fadeInCount: false,
+      days: "0",
+      hours: "00",
+      minutes: "00",
+      seconds: "00",
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.fadeInApp = true
+    }, 500)
+
+    setTimeout(() => {
+      this.fadeInCount = true
+    }, 2000)
+
+    setInterval(() => {
+      this.tick()
+    }, 1000)
+  },
+  computed: {
+    timer() {
+      const { days, hours, minutes, seconds } = this
+      return `${days}d ${hours}h ${minutes}m ${seconds}s`
+    },
+  },
+  methods: {
+    tick() {
+      const release = DateTime.fromISO("2019-11-08")
+      const duration = release.diffNow(["days", "hours", "minutes", "seconds"])
+
+      this.days = `${duration.days}`
+      this.hours = `${duration.hours}`.padStart(2, "0")
+      this.minutes = `${duration.minutes}`.padStart(2, "0")
+      this.seconds = `${Math.floor(duration.seconds)}`.padStart(2, "0")
+    },
+  },
+}
 </script>
 
-<style>
+<style scoped>
 #app {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url("./assets/bg_1.jpg");
+  background-position: center;
+  background-size: cover;
+  transition: opacity 1.5s ease;
+  opacity: 0;
+}
+#app.in {
+  opacity: 1;
+}
+.countdown-wrap {
+  position: absolute;
+  bottom: 30%;
+  width: 100%;
+  transition: all 1.5s ease;
+  opacity: 0;
+  transform: translateY(-20px);
+}
+.countdown-wrap.in {
+  opacity: 1;
+  transform: translateY(0);
+}
+.time {
+  font-size: 3.5rem;
+  color: #121619;
+  text-shadow: white 0px 0px 12px;
+}
+.copy {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  color: #ccc;
+  font-size: 0.8rem;
+}
+.copy > a {
+  color: #ccc;
+  text-decoration: none;
 }
 </style>
